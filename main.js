211a565dccaf507c395d8352f45e9acd866d0054
Barton Greenfield <BartonGreenfield@gmail.com>
2025-01-03 23:29:04 +1000

    add working screen, functioning 1.0.0

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 function add(num1, num2) {
     return num1 + num2;
 }
-
 function subtract(num1, num2) {
     return num1 - num2;
 }
-
 function multiply(num1, num2) {
     return num1 * num2;
 }
 function divide(num1, num2) {
-    return num1 / num2;
+    return (num1 / num2).toFixed(8);
 }
 
 function operate(num1, ope, num2) {
@@ -29,59 +27,75 @@ let operator;
 let secondNum;
 
 
+const screen = document.querySelector('.screen');
+
+
+
 
 const calculator = document.querySelector('.calculator-case');
 calculator.addEventListener('click', clickedButton);
-
 function clickedButton(e) {
     let value = e.target.textContent;
-    if (/^[0-9]$/.test(value)) {
+
+    if (/^[0-9]$/.test(value)) { 
         clickedNumber(value);
     }
     else if (value === '.') {
 
     }
     else if (value === 'A/C'){
-        firstNum = undefined;
-        operator = undefined;
-        secondNum = undefined;
-        
+        firstNum = operator = secondNum = undefined; 
+        screen.textContent = '0'; 
     }
     else if (value === '=') {
         clickedEqual(value);
-        
     }
-    else if (secondNum === undefined) {
-        operator =  value === '+' ? '+':
-                    value === "-" ? '-':
-                    value === "X" ? '*':
-                    '/';
-        console.log(operator);
+    else if ((secondNum === undefined) && ['+', '-', 'X', '÷'].includes(value)) {
+        clickedOperator(value);
     }
-    
-    
-    
 }
+
+
 
 function clickedNumber(value) {
     if (operator === undefined) {
-        firstNum === undefined ? firstNum = value : firstNum += value;
+        if (firstNum === undefined) {
+            firstNum = value;
+        }
+        else {
+            firstNum += value;
+        }
+        screen.textContent = firstNum; 
         
-        console.log("first num:" + firstNum);
     }
     else {
-        secondNum === undefined ? secondNum = value : secondNum += value;
-        console.log("second num:" + secondNum);
+        if (secondNum === undefined) {
+            secondNum = value;
+        }
+        else {
+            secondNum += value;
+        }
+        screen.textContent = firstNum + " " + operator + " " + secondNum;
+        
     }
-
+        
 };
+
+
 
 function clickedEqual(value) {
     if (!(firstNum === undefined || secondNum === undefined || operator === undefined)) {
-        console.log(operate(Number(firstNum), operator, Number(secondNum)));
+        screen.textContent = operate(Number(firstNum), operator, Number(secondNum));
         firstNum = operate(Number(firstNum), operator, Number(secondNum));
         secondNum = undefined;
         operator = undefined;
     }
     return
+}
+function clickedOperator(value) {
+    operator =  value === '+' ? '+':
+                value === "-" ? '-':
+                value === "X" ? '*':
+                '/';
+    screen.textContent = firstNum + " " + operator;
 }
