3e0ed627d0b9587ad23c35fdab015619cfb6e115
Barton Greenfield <BartonGreenfield@gmail.com>
2025-01-03 17:03:21 +1000

    add basic math functions

---
+++ main.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(1, 4));
+console.log(subtract(1, 4));
+console.log(multiply(1, 4));
+console.log(divide(1, 4));
