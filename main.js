5c2a25d9182e7a5018ae131b54c06f88283da7ff
Barton Greenfield <BartonGreenfield@gmail.com>
2025-01-03 22:27:35 +1000

    add backend calculations, interactive

--- main.js
+++ main.js
@@ -29,4 +29,56 @@ let operator;
 let secondNum;
 
 
-operate(5, "+", 2);
+
+const calculator = document.querySelector('.calculator-case');
+calculator.addEventListener('click', clickedButton);
+
+function clickedButton(e) {
+    let value = e.target.textContent;
+    if (/^[0-9]$/.test(value)) {
+        clickedNumber(value);
+    }
+    else if (value === '.') {
+
+    }
+    else if (value === 'A/C'){
+        firstNum = undefined;
+        operator = undefined;
+        secondNum = undefined;
+        console.log(firstNum);
+        console.log(operator);
+        console.log(secondNum);
+        
+    }
+    else if (value === '=') {
+        
+        console.log(operate(Number(firstNum), operator, Number(secondNum)));
+        firstNum = operate(Number(firstNum), operator, Number(secondNum));
+        secondNum = undefined;
+        operator = undefined;
+        
+    }
+    else if (secondNum === undefined) {
+        operator =  value === '+' ? '+':
+                    value === "-" ? '-':
+                    value === "X" ? '*':
+                    '/';
+        console.log(operator);
+    }
+    
+    
+    
+}
+
+function clickedNumber(value) {
+    if (operator === undefined) {
+        firstNum === undefined ? firstNum = value : firstNum += value;
+        
+        console.log("first num:" + firstNum);
+    }
+    else {
+        secondNum === undefined ? secondNum = value : secondNum += value;
+        console.log("second num:" + secondNum);
+    }
+
+};
